e7dba0be6256e6533c09390c451a121a0f68cd9a
sukui <diazordx@gmail.com>
2023-11-06 20:16:55 +0200

    remove cors for testing

--- core/core/settings.py
+++ core/core/settings.py
@@ -17,8 +17,6 @@ HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", default="*")
 
 ALLOWED_HOSTS = [HOSTS]
 
-CORS_ALLOW_ALL_ORIGINS = True
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -29,13 +27,11 @@ INSTALLED_APPS = [
 
     'base.apps.BaseConfig',
     'rest_framework',
-    "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
