33ba3d24aec68fff219964e75e4fd744ef5695ab
sukui <diazordx@gmail.com>
2023-11-07 19:03:30 +0200

    EAM-46 Added Swagger UI and static files

--- core/core/urls.py
+++ core/core/urls.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Sakuras Sweets API",
+        default_version='v1',
+        description="REST API. All endpoints provided by Sakuras Sweets API",
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('base.urls'))
+    path('', include('base.urls')),
+    # Swagger UI {
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0),
+         name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger',
+                                         cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc',
+                                       cache_timeout=0), name='schema-redoc'),
+    # }
 ]
