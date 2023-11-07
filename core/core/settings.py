631347f5adec666bd4cf120e20d18987e3d4c123
sukui <37778349+sukui24@users.noreply.github.com>
2023-11-07 19:07:03 +0200

    EAM-46 Added Swagger UI and static files

--- core/core/settings.py
+++ core/core/settings.py
@@ -34,6 +34,9 @@ INSTALLED_APPS = [
 
     # CORS Headers
     'corsheaders',
+
+    # Swagger UI
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static/',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
