695cf124b07833cdccb0d978b3c5c090abfa2a68
sukui <37778349+sukui24@users.noreply.github.com>
2023-11-08 15:48:51 +0200

    Update settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -134,7 +134,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     BASE_DIR / '/static/',
-    'app/static/
+    'app/static/'
 ]
 
 # Default primary key field type
