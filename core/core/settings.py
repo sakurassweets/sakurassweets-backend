795ceff61ff561c04ea614e57113b2597c11a9f0
sukui <37778349+sukui24@users.noreply.github.com>
2023-11-08 16:20:50 +0200

    Update settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -129,12 +129,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    BASE_DIR / '/staticfiles/',
-    'app/staticfiles/',
+    BASE_DIR / '/static/',
+    'app/static/',
 ]
 
 # Default primary key field type
