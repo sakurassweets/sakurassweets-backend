609fb610dff13496aed8dbc3960179e565bf06fd
sukui <diazordx@gmail.com>
2023-11-09 18:06:09 +0200

    EAM-44 feat: Added JWT support: Added /token/ endpoint to get tokens Added /refresh/ endpoint to refresh tokens

--- core/core/urls.py
+++ core/core/urls.py
@@ -4,6 +4,11 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 schema_view = get_schema_view(
     openapi.Info(
         title="Sakuras Sweets API",
@@ -26,4 +31,7 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc',
                                        cache_timeout=0), name='schema-redoc'),
     # }
+    # JWT token endpoints
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
