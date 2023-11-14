9f3bc1425327051e2a12a303179d5695c2e0f2eb
sukui <diazordx@gmail.com>
2023-11-14 09:38:55 +0200

    fix: PATCH method was acting same as PUT

--- core/user/views.py
+++ core/user/views.py
@@ -70,7 +70,7 @@ class UserViewSet(UpdateRetrieveDestroyListUserMixin,
 
         **Has permissions:** Anyone
         """
-        return super().update(request, *args, **kwargs)
+        return super().partial_update(request, *args, **kwargs)
 
     def destroy(self, request, *args, **kwargs):
         """
