e6d76b81a054f5584068a80d5e48e9d599aff692
sukui <diazordx@gmail.com>
2023-12-08 15:19:21 +0200

    refactor: Added permission and fixed user - Added permission for product, product type and price currencies:     - Permission grants viewing this models for everyone     - Permission grants adding/changing/deleting this models only for admins (staff or superuser)
    - Fixed delete method in user (in PRIVATE_ACTIONS it was named as 'delete' instead of 'destroy')

--- core/product/views.py
+++ core/product/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets
+
+from components.product import permissions as custom_permissions
 
 from product.models import Product, ProductType, PriceCurrency
 from product import serializers
@@ -7,16 +9,16 @@ from product import serializers
 class PriceCurrencyViewset(viewsets.ModelViewSet):
     queryset = PriceCurrency.objects.all()
     serializer_class = serializers.PriceCurrencySerializer
-    permission_classes = [permissions.AllowAny,]
+    permission_classes = [custom_permissions.IsAdminOrStaff,]
 
 
 class ProductTypeViewset(viewsets.ModelViewSet):
     queryset = ProductType.objects.all()
     serializer_class = serializers.ProductTypeSerializer
-    permission_classes = [permissions.AllowAny,]
+    permission_classes = [custom_permissions.IsAdminOrStaff,]
 
 
 class ProductViewset(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = serializers.ProductSerializer
-    permission_classes = [permissions.AllowAny,]
+    permission_classes = [custom_permissions.IsAdminOrStaff,]
