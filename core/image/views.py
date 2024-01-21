764ee220e491fb3a1f5ad7376456d04982b831cd
sukui <diazordx@gmail.com>
2024-01-21 21:14:38 +0200

    feat: Added little validation for image model

--- core/image/views.py
+++ core/image/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, exceptions
 
 from image.serializers import ImageSerializer
 from image.models import Image
@@ -7,4 +7,18 @@ from image.models import Image
 class ImageViewSet(viewsets.ModelViewSet):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
-    permission_classes = [permissions.AllowAny,]
+    permission_classes = [permissions.IsAdminUser()]
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [permissions.AllowAny()]
+        else:
+            return self.permission_classes
+
+    def create(self, request, *args, **kwargs):
+        related_to_id = request.data.get('related_to')
+        if Image.objects.filter(related_to=related_to_id, main_image=True).exists():
+            raise exceptions.APIException(
+                "This product already have main image")
+
+        return super().create(request, *args, **kwargs)
